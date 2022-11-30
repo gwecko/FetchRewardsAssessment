9b5abd53b741cd3dd1fa75e3502886af1fcc100e
Grant Wecker <grantwecker@live.com>
2022-11-29 21:12:30 -0500

    Complete functionality, refactoring

--- index.js
+++ index.js
@@ -7,92 +7,153 @@ const port = 3000
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', './')
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended: true}))
 
-// class Transaction {
-//     constructor(payer, points, timestamp) {
-//         this.payer = payer;
-//         this.points = points;
-//         this.timestamp = timestamp
-//     }
-// }
 
-// array for holding transaction objects
-const balances = []
+// balances array for holding payer balances.
+// only one object per payer.
+// transactions array for tracking transactions.
+// payer may have multiple objects (transactions) under 
+// their name. object is removed by spendPoints()
+const balances = [], transactions = [];
+
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', {transaction: balances})
+    res.render('index.ejs', {transaction: transactions})
 })
 
-// New transaction route (Payer, points, timestamp)
-// FINISHED
+// New transaction route
 app.post('/', (req, res) => {
-    
     // parse the information
     // everything in req.body is a string so we must convert 'points' to Number type
-    const { payer, points } = req.body
-    const pointsInt = parseInt(points)
+    const { payer } = req.body
+    const points = parseInt(req.body.points)
     const timestamp = req.body.timestamp || new Date(Date.now()).toUTCString()
     
     const transaction = {
-        payer: payer,
-        points: pointsInt,
-        timestamp: timestamp
+        payer: payer || undefined,
+        points: points || undefined,
+        timestamp: timestamp || undefined
     }
-    
-    processTransaction(transaction, balances)
+    if (transaction.points > 0) {
+        transactions.push(transaction)
+    }
+    processTransaction(transaction)
     res.render('index.ejs', {transaction: transaction})
+})
+
+// Spend points route
+app.post('/spend', (req, res) => {
+    let spentPoints = parseInt(req.body.spentPoints)
+    let spendResults = []
+    
+    spendPoints(spentPoints, spendResults)
+    
+    // res.render('spendresults.ejs', { spendResults: spendResults })
+    res.render('spendresults.ejs', {spendResults: spendResults})
 })
 
 // View balances route
 app.get('/balances', (req, res) => {
-    res.render('viewbalances.ejs', {balances: balances})
+    res.render('viewbalances.ejs', { balances: balances })
 })
 
 /**
- * 1. add transaction if balances[] is empty, else
+ * 
+ * 1. add transaction to balances[] if balances[] is empty, else
  * 2. check if payer has preexising balance and
  * 3(a). add or subtract from balance, or
  * 3(b). push new payer transaction if not preexisting
  *
- * @param {obj} t new transaction object
- * @param {array} balances pre-existing record
- * @returns void
+ * @param {Object} t new transaction object
  */
-function processTransaction(t, balances) {
-    // check for empty array
-    if (balances.length < 1) {
-        balances.push(t)
-    // check if preexisting payer exists
-    } else {
-        let match = false
-        let indexOfMatch = -1
-        while (!match) {
-            balances.forEach((entry, i) => {
-                if (entry.payer === t.payer) {
-                    match = true
-                    indexOfMatch = i
-                }
-                // set to true anyway at end of array to exit loop
-                match = true
-            })
+function processTransaction(t) {
+    // find if existing payer balance
+    let indexOfMatch = -1
+    for (let i = 0; i < balances.length; i++) {
+        if (balances[i].payer === t.payer) {
+            indexOfMatch = i
+            i += balances.length
         }
-        // add balance if payer account exists, otherwise add new payer & balance
-        if (indexOfMatch > -1) {
-            // prevent negative balance
-            if ((t.points < 0) && (balances[indexOfMatch].points + t.points) < 0) {
+    }
+    // if payer account exists...
+    if (indexOfMatch > -1) {
+        // ...check for negative points
+        if (t.points < 0) {
+            // Check to prevent negative balance:
+            if ((balances[indexOfMatch].points + t.points) < 0) {
                 t.payer = 'balance cannot become negative'
                 t.points = 0
                 t.timestamp = 0
             } else {
-                balances[indexOfMatch].points += t.points
+                // Negative points must be subtracted from the transactions 
+                // array (which keeps chronological order of points)
+                for (let i = 0; i < transactions.length; i++) {
+                    if (t.payer === transactions[i].payer) {
+                        transactions[i].points += t.points
+                        i += transactions.length
+                    }
+                }
             }
-        } else {
-            balances.push(t)
         }
+        // add (or subtract) value from existing payer
+        balances[indexOfMatch].points += t.points
+    } else {
+        // must create new object or balance[] and transactions[]
+        // will have pointers to the same address!
+        const newT = { ...t }
+        balances.push(newT)
     }
-    console.log('----------')
-    console.log(balances)
+}
+
+
+/** 
+ *
+ * @param {number} numPoints balance of points to be deducted from
+ * @param {array} spendLog for displaying every deduction that occurs
+ *
+ */
+function spendPoints(numPoints, spendLog) {
+    // while numPoints > 0
+    // Subtract oldest points from numPoints
+    
+        while (numPoints != 0) {
+            // if [i].points > numPoints then
+            // subtract numPoints from [i].points, change [i].points value
+            // and record the math
+            if (transactions[0].points > numPoints) {
+                transactions[0].points -= numPoints
+                let result = {
+                    payer: transactions[0].payer,
+                    points: `-${numPoints}`
+                }
+                spendLog.push(result)
+                numPoints = 0
+            } else {
+                // subtract [i].points from numPoints, remove transaction from array
+                numPoints -= transactions[0].points
+                let result = {
+                    payer: transactions[0].payer,
+                    points: `-${transactions[0].points}`
+                }
+                spendLog.push(result)
+                transactions.shift()
+            }
+        }
+    updateBalances()
+}
+
+
+/** 
+ * Provides updated payer balances after a points spend
+ */ 
+function updateBalances() {
+    // clear the existing balances
+    balances.splice(0, balances.length)
+    // register each transaction back into the balances array
+    transactions.forEach((transaction) => {
+        processTransaction(transaction)
+    })
 }
 
 app.listen(port, console.log(`Listening on port ${port}`))
